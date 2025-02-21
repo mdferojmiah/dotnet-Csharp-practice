9e19b5e902a832d83bd83cebf7b191de70affba0
Feorj Miah <01ferojmiah@gmail.com>
2025-02-22 03:43:23 +0600

    created a console app for practicing C#

---
+++ FristConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Program{
+    public static void Main(string[] args){
+        string userName = "Feroj Miah";
+        int age = 24;
+        double salary = 555.55;
+        char bloodGroup = 'O';
+        bool isAvailabe = true;
+
+        int num1, num2, num3;
+        num1 = num2 = num3 = 5;
+
+        double num4 = (int) num1;
+        string strNum = Convert.ToString(num2);
+
+        Console.WriteLine(userName);
+        Console.WriteLine(age);
+        Console.WriteLine(salary + "$");
+        Console.WriteLine(bloodGroup);
+        Console.WriteLine(isAvailabe);
+
+        Console.WriteLine(num1 + " " + num2 + " " + num3);
+        Console.WriteLine(num4);
+        Console.WriteLine(strNum);
+    }
+}
