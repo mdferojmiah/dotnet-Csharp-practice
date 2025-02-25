4bd416fa53f7684120049d9d0dd8b3aa06c606b8
Feorj Miah <01ferojmiah@gmail.com>
2025-02-25 10:33:24 +0600

    looping folder is being added! loop and nested loop is being practiced here!

---
+++ Loop/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+class Looping{
+    public static void Main(string[] args){
+        //Factorial();
+        //Multiplication();
+        Multi();
+    }
+
+
+    static void Factorial(){
+        Console.Write("Enter a number: ");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int fact = 1;
+
+        for(int i = 1; i <= number; i++){
+            fact *= i;
+        }
+
+        Console.WriteLine(fact);
+    }
+
+
+    static void Multiplication(){
+        Console.Write("Enter a number: ");
+        int input = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = 1; i <= 10; i++){
+            Console.WriteLine($"{input} x {i} = {input*i}");
+        }
+    }
+
+    static void Multi(){
+        Console.Write("Start: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("End: ");
+        int end = Convert.ToInt32(Console.ReadLine());
+
+        for(int i = start; i <= end; i++){
+            for(int j = 1; j <= 10; j++){
+                Console.WriteLine($"{i} x {j} = {i * j}");
+            }
+            Console.WriteLine();
+        }
+    }
+}
